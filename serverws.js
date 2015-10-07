bee2bf56248919719dbbc3521d550d627548bf96
quentinkornmann <quentin.kornmann@sfr.fr>
2015-10-07 17:25:51 +0200

    server websocket en cours

--- serverws.js
+++ serverws.js
@@ -2,6 +2,7 @@ var express = require('express');
 var https = require('https');
 var fs = require('fs');
 var app = express();
+var port = 8000;
 
 app.use(express.static(__dirname+"/views"));
 
@@ -14,30 +15,48 @@ var options = {
 
 var server = https.createServer(options, app);
 
-server.listen(8000);
+server.listen(port);
+console.log('Server listening on port ' + port);
 
 var WebSocketServer = require('ws').Server, wss = new WebSocketServer({ server : server });
 var id = 0;
-//var clients = {};
-var v:
+var sockets = [];
+var v;
 
 wss.on('connection', function connection(ws) {
-  console.log('connection ' + id + ' établie');
-  //clients[id] = connection;
-  id++;
-  
-  ws.on('message', function (message){
-    console.log('received: %s', message);
-	ws.send('reponse');
-  });
+	console.log('connection ' + id + ' établie');
+	sockets[id] = ws;
+	ws.send('init');
+	console.log('init sent ' + id);
+	id++;
+	
+	ws.on('message', function(e){
+		// traitement requête client
+	});
+	
+	ws.on('close', function(){
+		for(var i = 0 ; i < sockets.length ; i++){
+			if(sockets[i] = ws){
+				sockets.splice(i, 1);
+			}
+		}
+	});
 });
- 
-console.log('Server listening on port 8000');
 
+f();
 function f() {
-	v = setInterval(send, 16);
-}
+	v = setInterval(send, 2000);
+};
 
 function send() {
-
-}
+	var msg = 'test';
+	
+	for(var i = 0 ; i < sockets.length ; i++)
+	{
+		if(sockets[i] != null)
+		{
+			sockets[i].send(msg);
+			console.log(msg + ' sent');
+		}
+	}
+};	
