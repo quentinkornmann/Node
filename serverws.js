9e06c813c5813bb5d61240058702111fabf40bd8
quentinkornmann <quentin.kornmann@sfr.fr>
2015-10-21 17:00:20 +0200

    travail sur le multijoueur en cours

--- serverws.js
+++ serverws.js
@@ -20,51 +20,106 @@ console.log('Server listening on port ' + port);
 
 var WebSocketServer = require('ws').Server, wss = new WebSocketServer({ server : server });
 var sockets = [];
-var lastid = 0, v, dx, dy, res, norm;
+var snakes = [];
+var dxs = [];
+var dys = [];
+var norms = [];
+var lastid = 0, v;
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws){
 	console.log('connection ' + lastid + ' établie');
 	sockets.push(ws);
 	ws.send('init');
-	console.log('init sent ' + lastid);
+	console.log('init envoyé au client ' + lastid);
 	lastid++;
-	f();
 	
 	ws.on('message', function(msg){
-
-		res = msg.split("/");
-		dx = res[2] - res[0];
-		dy = res[3] - res[1];
-		norm = Math.sqrt((res[3] - res[1]) * (res[3] - res[1]) + (res[2] - res[0]) * (res[2] - res[0]));
-		res.splice(res.length - 2, 2);
-		console.log('dx : ' + dx);
-		console.log('dy : ' + dy);		
+		var norm, res, dx, dy;
+		var t = [];
+		res = msg.split("/");	// BON
+			
+		if(res[20] != null && res[21] != null){
+			dx = res[20] - res[0];
+			dy = res[21] - res[1];
+			norm = Math.sqrt(dy * dy + dx * dx);
+			res.splice(res.length - 2, 2);
+			for(var i = 0 ; i < sockets.length ; i++){
+				if(sockets[i] == ws){
+					for(j = 0 ; j < res.length ; j++){
+						snakes[i][j] = parseFloat(res[j]);
+					}
+					dxs[i] = dx;
+					dy[i] = dy;
+					norms[i] = norm;
+				}
+			}
+		}
+		else{
+			for(var i = 0 ; i < sockets.length ; i++){
+				if(sockets[i] == ws){
+					snakes[i][0] = parseFloat(res[0]);
+					snakes[i][1] = parseFloat(res[1]);
+				}
+			}
+		}
 	});
 	
 	ws.on('close', function(){
 		for(var i = 0 ; i < sockets.length ; i++){
 			if(sockets[i] == ws){
-				sockets.splice(i, 1);
 				console.log('client ' + i + ' déconnecté');
 			}
 		}
 	});
 });
 
-function f() {
+f();
+function f(){
 	v = setInterval(send, 50);
 };
+
+function send(){
+	var res = "";
+
+	calculer();
 	
-function send() {
-	var x, y;
-	if(res != null){
-		x = parseFloat(res[0]);
-		x += dx / (norm / 5);
-		res[0] = x.toString();
-		y = parseFloat(res[1]);
-		y += dy / (norm / 5);
-		res[1] = y.toString();
-		
-		sockets[lastid - 1].send("upda" + res.toString());
+	for(var i = 0 ; i < snakes.length ; i++){
+		for(var j = 0 ; j < snakes[i].length ; j++){
+			res += snakes[i][j].toString() + '/';
+		}
+		res+= "|";
 	}
-};	
+	
+	for(var i = 0 ; i < sockets.length ; i++){
+		if(sockets[i].readyState == 1){
+			sockets[i].send("upda" + res); 
+		}
+	}
+};
+
+function calculer(){
+	var x, y;
+	for(var i = 0 ; i < snakes.length ; i++){
+		x = snakes[i][0];
+		x += dx[i] / (norms[i] / 5);
+		y = snakes[i][1];
+		y += dy[i] / (norms[i] / 5);
+		
+		if(x <= 0){
+			x += 1000;
+		}
+		if(x >= 1000){
+			x -= 1000
+		}
+			
+		if(y <= 0){
+			y += 400;
+		}				
+		if(y >= 400){
+			y -= 400;
+		}
+		
+		snakes[i][0] = x;
+		snakes[i][1] = y;
+	}
+}
